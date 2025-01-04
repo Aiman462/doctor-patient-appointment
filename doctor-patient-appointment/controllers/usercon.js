999dd61105d4c01ecd4349390c4ff7994e07504c
Aiman462 <aime4621@gmail.com>
2025-01-04 05:42:11 -0800

    removed token

--- doctor-patient-appointment/controllers/usercon.js
+++ doctor-patient-appointment/controllers/usercon.js
@@ -3,11 +3,11 @@ import User from "../schemas/userschema.js";
 let Usercontroller={
     create: async(req,res)=>{
         try{
-            const user=new User({...req.body,confirmtoken:req.headers.authorization,Token:"user123"});
+            const user=new User({...req.body});
             await user.save();
             res.status(201).send({ message:"User created", user});
         }catch(error){
-            res.status(401).send({ message:"you are unauthorized", error});
+            res.status(400).send({ message:"Error creating user", error});
         }
     }
 };
--- doctor-patient-appointment/schemas/userschema.js
+++ doctor-patient-appointment/schemas/userschema.js
@@ -4,14 +4,6 @@ const userSchema= new mongoose.Schema({
      Name:{type:String , required:true},
      Age: {type:Number , required:true},
      Profile:{type:String , enum:["patient","doctor"], required:true},
-     Token:{type:String , required:true},
-     confirmtoken:{type:String , required:true,
-        validation:{
-            validator: function(value){
-                value===this.Token
-            }
-        }
-     }
 
 });
 
